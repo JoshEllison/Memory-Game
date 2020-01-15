ab8b52a6eb020c6b027e84560a2dc3e763f29267
docholliday211 <35431728+docholliday211@users.noreply.github.com>
2020-01-15 14:51:55 -0800

    second android app finished

---
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.memorygame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    //for high score
+    SharedPreferences prefs;
+    String dataName = "MyData";
+    String intName = "MyInt";
+    int defaultInt = 0;
+    int highScore;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //for high score
+        //initialize two SharedPreferences objects
+        prefs = getSharedPreferences(dataName,MODE_PRIVATE);
+
+        //Either load High score or
+        //if not available our default of 0
+        highScore = prefs.getInt(intName, defaultInt);
+
+        //Make a reference to the Highscore textview in layout
+        TextView textHighScore =(TextView) findViewById(R.id.textHighScore);
+        //Display the hi score
+        textHighScore.setText("High: "+ highScore);
+
+        //Make a button from the button in layout
+        Button button =(Button) findViewById(R.id.button);
+
+        //Make each listen for clicks
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
+
+    }
+}
